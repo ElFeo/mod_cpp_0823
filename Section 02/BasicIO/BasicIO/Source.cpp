709589c4c15e5cba566f276016ffb5f9208a5efb
Andrzej Jakubowski <ajakubowski3@gatech.edu>
2024-05-14 17:39:05 -0600

    initial commit 05/14/2024 - clean up and mods

--- Section 02/BasicIO/BasicIO/Source.cpp
+++ Section 02/BasicIO/BasicIO/Source.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
 	using namespace std;
 	
-	char buff[512];
+	char buff[64];
 	cout << "What is your name?";
 	/*
 	There's a better way to do it. Check the 
@@ -12,7 +13,10 @@ int main() {
 	cin.getline(buff, 64, '\n');
 	cout << "Your name is:" << buff << endl;
 	
-	int age ;
-	cin >> age ;
+	int age{ 0 };
+	std::cout << "age variable after init = " << std::to_string(age) << std::endl;
+	std::cout << "What is your age?" << std::endl;
+	std::cin >> age;
+	std::cout << "your age is: " << std::to_string(age) << std::endl;
 	return 0;
 }
